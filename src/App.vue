e6faa84685100e9ed4b4479c1c5abc0c5e5504a3
Ryan Paige <oshmagoshzz@gmail.com>
2021-02-28 22:37:40 -0500

    footer added

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <my-header></my-header>
     <h1>Hello from root vue component</h1>
+    <my-footer></my-footer>
   </div>
 </template>
 
 <script>
-import Header from './components/AppHeader.vue'
+import Header from '@/components/AppHeader.vue'
+import Footer from '@/components/AppFooter.vue'
 
 export default {
   components: {
-    'my-header': Header
+    'my-header': Header,
+    'my-footer': Footer
   }
 }
 </script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p id='date'>Copyright &copy; {{getYear}} </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  computed: {
+    getYear: function () {
+        return new Date().getFullYear()   
+    }
+  }
+}
+</script>
