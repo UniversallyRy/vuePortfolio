03eb2e3489b0c9f9ca60f470b578b5b623b7cfbd
Ryan Paige <oshmagoshzz@gmail.com>
2021-03-22 20:31:46 -0400

    comment added

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,6 +10,7 @@
 <script>
 // todos: decide if blog will be priv/publ, diagonal header, better spacing, more projects
 // style page(spawn/cool tone colors?), add more components and optimize code and site speed
+// Dream Journal Component needed
 import About from "@/components/About.vue"
 import Skills from "@/components/Skills.vue"
 import Projects from "@/components/Projects.vue"
