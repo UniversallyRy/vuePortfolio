fde341b5712bc05745c34536f0a94d5853609653
Ryan Paige <oshmagoshzz@gmail.com>
2021-12-22 22:10:19 -0500

    skills added

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -26,24 +26,24 @@
             <p 
               class="mb-1 subtitle-1 text-center"
             >
-                  <v-icon 
-                    @click="skill.expand=!skill.expand" 
-                    left 
-                    v-on="listeners"
-                    v-bind="attrs"
-                    :color="skill.color"
-                  >
-                    {{ skill.icon }}
-                  </v-icon>
-                {{ skill.name }}
-                <v-expand-transition>
-                  <v-card
-                    v-show="skill.expand"
-                    height="100"
-                    width="100"
-                    class="mx-auto secondary"
-                  ></v-card>
-                </v-expand-transition>
+              <v-icon 
+                @click="skill.expand=!skill.expand" 
+                left 
+                v-on="listeners"
+                v-bind="attrs"
+                :color="skill.color"
+              >
+                {{ skill.icon }}
+              </v-icon>
+              {{ skill.name }}
+              <v-expand-transition>
+                <v-card
+                  v-show="skill.expand"
+                  height="100"
+                  width="100"
+                  class="mx-auto secondary"
+                ></v-card>
+              </v-expand-transition>
             </p>
           </template>
           <span>Click Icon To Toggle Info</span>
@@ -57,106 +57,153 @@
 export default {
   data: () => ({
     skills: [
-        {
-            name: "JavaScript",
-            icon: "mdi-language-javascript",
-            color: "#F0DB4F",
-            hover: false,
-            expand: false,
-        },
-        {
-            name: "HTML",
-            icon: "mdi-language-html5",
-            color: "#e34c26",
-            hover: false,
-            expand: false,
-        },
-        {   name: "CSS", 
-            icon: "mdi-language-css3",
-            color: "#2965f1", 
-            hover: false,
-            expand: false, 
-        },
-        {   name: "React", 
-            icon: "mdi-react", 
-            color: "#61DBFB", 
-            hover: false,
-            expand: false, 
-        },
-        {
-            name: "React Native",
-            icon: "mdi-react",
-            color: "#61DBFB",
-            hover: false,
-            expand: false,
-        },
-        {
-            name: "React Redux",
-            icon: "mdi-react",
-            color: "#764abc ",
-            hover: false,
-            expand: false,
-        },
-        {   name: "NodeJS", 
-            icon: "mdi-nodejs", 
-            color: "#3c873a", 
-            hover: false,
-            expand: false, 
-        },
-        {   name: "Vue.js", 
-            icon: "mdi-vuejs", 
-            color: "#41B883", 
-            hover: false,
-            expand: false, 
-        },
-        {   name: "Vuetify", 
-            icon: "mdi-vuetify", 
-            color: "#42A5F5", 
-            hover: false,
-            expand: false,
-        },
-        {
-            name: "GraphQL",
-            icon: "mdi-graphql",
-            color: "#e535ab",
-            hover: false,
-            expand: false,
-        },
-        {
-            name: "Expo",
-            icon: "mdi-chevron-up",
-            color: "#000",
-            hover: false,
-            expand: false,
-        },
-        {
-            name: "Bootstrap",
-            icon: "mdi-bootstrap",
-            color: "#563d7c",
-            hover: false,
-            expand: false,
-        },
-        {
-            name: "Python",
-            icon: "mdi-language-python",
-            color: "#306998",
-            hover: false,
-            expand: false,
-        },
-        {
-            name: "Jest",
-            icon: "mdi-ab-testing",
-            color: "#15c213",
-            hover: false,
-            expand: false,
-        },
-        {
-            name: "TypeScript",
-            icon: "mdi-language-typescript",
-            color: "#719af4",
-            hover: false,
-            expand: false,
-        },
+      {
+        name: "JavaScript",
+        icon: "mdi-language-javascript",
+        color: "#F0DB4F",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "HTML",
+        icon: "mdi-language-html5",
+        color: "#e34c26",
+        hover: false,
+        expand: false,
+      },
+      {   
+        name: "CSS", 
+        icon: "mdi-language-css3",
+        color: "#2965f1", 
+        hover: false,
+        expand: false, 
+      },
+      {   
+        name: "React", 
+        icon: "mdi-react", 
+        color: "#61DBFB", 
+        hover: false,
+        expand: false, 
+      },
+      {
+        name: "React Native",
+        icon: "mdi-react",
+        color: "#61DBFB",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "React Redux",
+        icon: "mdi-react",
+        color: "#764abc ",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "TypeScript",
+        icon: "mdi-language-typescript",
+        color: "#719af4",
+        hover: false,
+        expand: false,
+      },
+      {   
+        name: "Vue.js", 
+        icon: "mdi-vuejs", 
+        color: "#41B883", 
+        hover: false,
+        expand: false, 
+      },
+      {   
+        name: "Vuetify", 
+        icon: "mdi-vuetify", 
+        color: "#42A5F5", 
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "Native-base",
+        icon: "mdi-palette",
+        color: "#e535ab",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "Chakra-UI",
+        icon: "mdi-palette",
+        color: "#e535ab",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "styled-components",
+        icon: "mdi-palette",
+        color: "#e535ab",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "Reanimated",
+        icon: "mdi-animation",
+        color: "#e535ab",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "Moti",
+        icon: "mdi-animation",
+        color: "#e535ab",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "Framer-motion",
+        icon: "mdi-animation",
+        color: "#e535ab",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "GraphQL",
+        icon: "mdi-graphql",
+        color: "#e535ab",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "Expo",
+        icon: "mdi-chevron-up",
+        color: "#000",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "Bootstrap",
+        icon: "mdi-bootstrap",
+        color: "#563d7c",
+        hover: false,
+        expand: false,
+      },
+      {
+        name: "Python",
+        icon: "mdi-language-python",
+        color: "#306998",
+        hover: false,
+        expand: false,
+      },
+      {   
+        name: "NodeJS", 
+        icon: "mdi-nodejs", 
+        color: "#3c873a", 
+        hover: false,
+        expand: false, 
+      },
+      {
+        name: "Jest",
+        icon: "mdi-ab-testing",
+        color: "#15c213",
+        hover: false,
+        expand: false,
+      },
     ],
   }),
 };
